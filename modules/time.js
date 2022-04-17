79fe8796594ae4f8966af049b4959ebc76099342
Jake Lim <limhm1396@naver.com>
2022-04-17 13:50:38 +0900

    기본 Life Time 가져오는 함수 추가

---
+++ modules/time.js
@@ -0,0 +1,14 @@
+exports.getTTL = () => {
+    const date = new Date();
+    const newDate = new Date();
+
+    newDate.setDate(newDate.getDate() + 1);
+    newDate.setHours(0);
+    newDate.setMinutes(0);
+    newDate.setSeconds(1);
+    newDate.setMilliseconds(0);
+
+    const diff_second = (newDate - date) / 1000;
+
+    return Math.floor(diff_second);
+};
