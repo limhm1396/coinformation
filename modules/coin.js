9cecceaf9f511141a09b9cbd5deab10a5c84afb6
Jake Lim <limhm1396@naver.com>
2022-04-17 13:50:58 +0900

    대대적인 모듈 수정

--- modules/coin.js
+++ modules/coin.js
@@ -1,27 +1,42 @@
 const fetch = require('node-fetch');
 
+const redis = require('../redis');
+const { getTTL } = require('./time');
+
+const UPBIT = require('../common/upbit');
+
 const { Market } = require('../models');
 
-class Upbit {
+class Coin {
+    /**
+     * @description 업비트에서 마켓 목록 가져오기
+     * @returns {Array} markets
+     */
     static getKrwMarketsFromUpbit = async () => {
-        const url = 'https://api.upbit.com/v1/market/all?isDetails=false';
-        const options = { method: 'GET', headers: { Accept: 'application/json' } };
+        const url = UPBIT.UPBIT_MARKET_CODE_LOOKUP;
+        const options = UPBIT.GET_OPTIONS;
 
         const res = await fetch(url, options);
-        const coins = await res.json();
+        const data = await res.json();
+        const coins = Array.from(data); // 밑에 filter에서 변수명이 겹치지 않기 위해 이렇게 만듬.
 
-        return coins.reduce((result, coin) => {
+        const markets = coins.filter((coin) => {
             const market = String(coin.market);
-            if (market.startsWith('KRW')) {
-                result.push(coin);
-            }
-            return result;
-        }, []);
+            return market.startsWith('KRW');
+        });
+
+        return markets;
     };
 
-    static postMarkets = async (coins) => {
+    /**
+     * @description 업비트에서 가져온 마켓 목록 저장하기
+     */
+    static postMarkets = async () => {
+        const coins = await this.getKrwMarketsFromUpbit(); // 밑에 for문에서 변수명이 겹치지 않기 위해 이렇게 만듬.
+
         for (let coin of coins) {
             const market = String(coin.market);
+            const ticker = market.slice(4);
             const korean_name = String(coin.korean_name);
             const english_name = String(coin.english_name);
 
@@ -31,6 +46,7 @@ class Upbit {
                 },
                 defaults: {
                     market,
+                    ticker,
                     korean_name,
                     english_name,
                 },
@@ -38,57 +54,108 @@ class Upbit {
         }
     }
 
+    /**
+     * @description DB에서 마켓 목록 가져오기
+     * @returns {Array} markets
+     */
     static getMarkets = async () => {
-        const makrets = await Market.findAll();
-        return makrets;
+        const markets = await redis.get('markets');
+
+        if (!markets) {
+            await this.postMarkets();
+
+            const markets = await Market.findAll();
+
+            await redis.set('markets', JSON.stringify(markets), {
+                EX: getTTL(),
+            })
+
+            return markets;
+        } else {
+            return JSON.parse(markets);
+        }
     }
 
+    /**
+     * @description markets[]에서 마켓코드만 추출해 Array로 반환하기
+     * @param {Market[]} markets require
+     * @returns {String[]} markets_code[]
+     */
     static getMarketsCode = (markets) => {
-        return markets.reduce((result, market) => {
+        return markets.map((market) => {
             const code = String(market.market);
-            result.push(code);
-            return result;
-        }, []);
+            return code;
+        });
     }
 
+    /**
+     * @description 업비트에서 마켓 시세 정보 가져오기
+     * @param {Market[]} markets
+     * @returns {Array} infos
+     */
     static getMarketsInfoFromUpbit = async (markets) => {
         if (!markets) {
-            markets = await this.getKrwMarketsFromUpbit();
+            markets = await this.getMarkets();
         }
-    
-        const base_url = 'https://api.upbit.com/v1/ticker?markets=';
-        const options = { method: 'GET', headers: { Accept: 'application/json' } };
-    
+
+        const base_url = UPBIT.UPBIT_MARKET_CURRENT_INFO;
+        const options = UPBIT.GET_OPTIONS;
+
         const codes = this.getMarketsCode(markets);
         const full_url = base_url + String(codes);
-    
+
         const res = await fetch(full_url, options);
-        const infos = await res.json();
-    
+        const data = await res.json();
+        const infos = Array.from(data);
+
         return infos;
     }
 
-    static getMarketsMDD = async (infos) => {
-        if (!infos) {
-            infos = await this.getMarketsInfoFromUpbit();
-        }
-    
-        // 최대낙폭지수 퍼센트 공식: (현재가 - 최고가) / 최고가 * 100
-        return infos.reduce((result, info) => {
-            const market = String(info.market);
-            const current_price = Number(info.trade_price);
-            const highest_price = Number(info.highest_52_week_price);
-    
-            const mdd = ((current_price - highest_price) / highest_price * 100).toFixed(2);
-    
-            result.push({
-                market,
-                mdd,
+    /**
+     * @description 마켓 정보, 현 시세와 계산된 MDD 값이 포함된 Array 반환
+     * @returns {Array} market_mdds
+     */
+    static getMarketsMDD = async () => {
+        const market_mdds = await redis.get('markets_mdd');
+
+        if (!market_mdds) {
+            const infos = await this.getMarketsInfoFromUpbit();
+
+            const market_mdds = [];
+            for (let info of infos) {
+                const market = await Market.findOne({
+                    where: {
+                        market: String(info.market),
+                    }
+                });
+
+                const korean_name = market.korean_name;
+                const english_name = market.english_name;
+                const current_price = Number(info.trade_price);
+                const highest_price = Number(info.highest_52_week_price);
+
+                // 최대낙폭지수 퍼센트 공식: (현재가 - 최고가) / 최고가 * 100
+                const mdd = ((current_price - highest_price) / highest_price * 100).toFixed(2);
+
+                market_mdds.push({
+                    market: market.market,
+                    korean_name,
+                    english_name,
+                    current_price,
+                    highest_price,
+                    mdd,
+                });
+            }
+
+            await redis.set('markets_mdd', JSON.stringify(market_mdds), {
+                EX: 1,
             });
-    
-            return result;
-        }, []);
+
+            return market_mdds;
+        } else {
+            return JSON.parse(market_mdds);
+        }
     }
 }
 
-module.exports = Upbit;
+module.exports = Coin;
