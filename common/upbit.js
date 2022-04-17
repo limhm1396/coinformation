5bee30f9147d03844770fdd07397d29bcbf97c6a
Jake Lim <limhm1396@naver.com>
2022-04-17 21:45:10 +0900

    상세 정보 function 추가

--- common/upbit.js
+++ common/upbit.js
@@ -1,3 +1,4 @@
 exports.UPBIT_MARKET_CODE_LOOKUP = 'https://api.upbit.com/v1/market/all?isDetails=false';
 exports.UPBIT_MARKET_CURRENT_INFO = 'https://api.upbit.com/v1/ticker?markets=';
+exports.UPBIT_MARKET_CANDLE_DAYS = 'https://api.upbit.com/v1/candles/days?';
 exports.GET_OPTIONS = { method: 'GET', headers: { Accept: 'application/json' } };
--- modules/coin.js
+++ modules/coin.js
@@ -2,10 +2,14 @@ const fetch = require('node-fetch');
 
 const redis = require('../redis');
 const { getTTL } = require('./time');
+const { sequelize } = require('../models');
 
 const UPBIT = require('../common/upbit');
 
-const { Market } = require('../models');
+const {
+    Market,
+    MarketHistory,
+} = require('../models');
 
 class Coin {
     /**
@@ -31,7 +35,7 @@ class Coin {
     /**
      * @description 업비트에서 가져온 마켓 목록 저장하기
      */
-    static postMarkets = async () => {
+    static updateMarkets = async () => {
         const coins = await this.getKrwMarketsFromUpbit(); // 밑에 for문에서 변수명이 겹치지 않기 위해 이렇게 만듬.
 
         for (let coin of coins) {
@@ -62,7 +66,7 @@ class Coin {
         const markets = await redis.get('markets');
 
         if (!markets) {
-            await this.postMarkets();
+            await this.updateMarkets();
 
             const markets = await Market.findAll();
 
@@ -112,6 +116,17 @@ class Coin {
     }
 
     /**
+     * @description MDD 계산
+     * @param {Number} price 현재가
+     * @param {Number} h_price 최고가
+     * @returns {String} mdd(최대 소숫점 2자리까지)
+     */
+    static getMdd = (price, h_price) => {
+        // 최대낙폭지수 퍼센트 공식: (현재가 - 최고가) / 최고가 * 100
+        return ((price - h_price) / h_price * 100).toFixed(2);
+    }
+
+    /**
      * @description 마켓 정보, 현 시세와 계산된 MDD 값이 포함된 Array 반환
      * @returns {Array} market_mdds
      */
@@ -133,9 +148,7 @@ class Coin {
                 const english_name = market.english_name;
                 const current_price = Number(info.trade_price);
                 const highest_price = Number(info.highest_52_week_price);
-
-                // 최대낙폭지수 퍼센트 공식: (현재가 - 최고가) / 최고가 * 100
-                const mdd = ((current_price - highest_price) / highest_price * 100).toFixed(2);
+                const mdd = this.getMdd(current_price, highest_price);
 
                 market_mdds.push({
                     market: market.market,
@@ -154,6 +167,134 @@ class Coin {
             return market_mdds;
         } else {
             return JSON.parse(market_mdds);
+        }
+    }
+
+    /**
+     * @description CandleUrl(생성자 함수)
+     * @param {String} url 
+     * @param {String} market 
+     * @param {String} to 
+     * @param {String} count
+     */
+    static CandleUrl = function (url = '', market = '', to = '') {
+        this.url = url;
+        this.market = market;
+        this.to = to;
+        this.count = 200;
+        this.maxCount = this.count;
+
+        this.getUrl = () => {
+            return `${this.url}market=${this.market}&to=${this.to}&count=${this.count}`;
+        }
+    }
+
+    /**
+     * @description 마켓 히스토리 조회
+     * @param {String} marketCode e.g. KRW-BTC
+     * @returns {MarketHistory[]}
+     */
+    static getMarketHistories = async (marketCode) => {
+        await this.updateMarketHistories(marketCode);
+
+        return await MarketHistory.findAll({
+            where: {
+                market: marketCode,
+            },
+            order: [['date', 'ASC']],
+        });
+    }
+
+    /**
+     * @description 마켓 히스토리 업데이트
+     * @param {String} marketCode e.g. KRW-BTC
+     */
+    static updateMarketHistories = async (marketCode) => {
+        const history = await MarketHistory.findOne({
+            where: {
+                market: marketCode,
+            },
+            order: [['date', 'DESC']],
+        });
+
+        const url = UPBIT.UPBIT_MARKET_CANDLE_DAYS;
+        const options = UPBIT.GET_OPTIONS;
+
+        const date = new Date();
+        date.setDate(date.getDate() - 1);
+
+        const candleUrl = new this.CandleUrl(url, marketCode, date.toISOString());
+
+        if (history) {
+            candleUrl.isFirstTime = false;
+
+            const latest_date = new Date(history.date);
+
+            const diff = date - latest_date;
+            const temp = diff / (24 * 60 * 60 * 1000);
+            const count = Math.floor(temp);
+
+            candleUrl.count = String(count);
+        } else {
+            candleUrl.isFirstTime = true;
+        }
+
+        const histories = [];
+        let length = 0;
+        do {
+            const res = await fetch(candleUrl.getUrl(), options);
+            const json = await res.json();
+            const data = Array.from(json);
+
+            histories.push(...data);
+
+            const str_date = data[data.length - 1].candle_date_time_utc;
+            const date = new Date(str_date)
+            candleUrl.to = date.toISOString();
+
+            length = data.length;
+
+            if (!candleUrl.isFirstTime) {
+                candleUrl.count -= length;
+            }
+        } while (length >= candleUrl.maxCount);
+
+        histories.reverse(); // 가장 오래된 날짜부터 최고가 계산하기 위함.
+
+        let highest_price;
+        if (candleUrl.isFirstTime) {
+            highest_price = 0;
+        } else {
+            highest_price = history.highest_price;
+        }
+
+        const transaction = await sequelize.transaction();
+        try {
+            for (let history of histories) {
+                const date = new Date(history.candle_date_time_kst);
+                const trade_price = history.trade_price;
+                highest_price = Math.max(history.high_price, highest_price);
+                const mdd = this.getMdd(trade_price, highest_price);
+
+                await MarketHistory.findOrCreate({
+                    where: {
+                        market: marketCode,
+                        date,
+                    },
+                    defaults: {
+                        market: marketCode,
+                        date,
+                        trade_price,
+                        highest_price,
+                        mdd,
+                    },
+                    transaction,
+                });
+            }
+            await transaction.commit();
+        } catch (err) {
+            console.log(err);
+            await transaction.rollback();
         }
     }
 }
