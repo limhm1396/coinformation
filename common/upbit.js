5ec11c4374192727f95e6028d3b54638c1787973
Jake Lim <limhm1396@naver.com>
2022-04-17 13:49:30 +0900

    upbit 관련 고정값

---
+++ common/upbit.js
@@ -0,0 +1,3 @@
+exports.UPBIT_MARKET_CODE_LOOKUP = 'https://api.upbit.com/v1/market/all?isDetails=false';
+exports.UPBIT_MARKET_CURRENT_INFO = 'https://api.upbit.com/v1/ticker?markets=';
+exports.GET_OPTIONS = { method: 'GET', headers: { Accept: 'application/json' } };
