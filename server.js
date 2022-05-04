6dee479c7dd9d5b865aefc45b3b19df1b817d5b8
Jake Lim <limhm1396@naver.com>
2022-05-04 23:32:55 +0900

    알림봇 수정 - DB/redis 연결할 때, port 열 때 각각 알림

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
 const app = require('./app');
 
-// server open
+const { WebhookClient } = require('discord.js');
+const webhook = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_SERVER_RESTART_BOT_URL });
+
 const { sequelize } = require('./models');
 const redis = require('./redis');
-sequelize.sync({ force: false, alter: false })
-  .then(() => {
-    redis.connect()
-      .then(async () => {
-        // cron
-        const cron = require('./cron');
 
-        const main_cache = await redis.get('GET:MARKETS');
-        if (!main_cache) {
-          // market update
-          const Coin = require('./modules/coin');
-          await Coin.updateAllMarketHistories();
-        }
+// server start
+(async () => {
+  // db connect
+  try {
+    await sequelize.sync({ force: false, alter: false });
+    console.log('DB start');
+    await webhook.send({ content: new Date().toISOString() + ' / DB start' });
+  } catch (err) {
+    console.log('DB error');
+    await webhook.send({ content: new Date().toISOString() + ' / DB error' });
+  }
 
-        app.listen(app.get('port'), () => {
-          const { WebhookClient } = require('discord.js');
-          new WebhookClient({ url: process.env.DISCORD_WEBHOOK_SERVER_RESTART_BOT_URL })
-            .send({ content: 'Server Start' });
-          console.log(app.get('port'), 'port server is running!');
-        });
-      })
-  }).catch(() => console.log('ERROR'));
+  // redis connect
+  try {
+    await redis.connect();
+    console.log('redis start');
+    await webhook.send({ content: new Date().toISOString() + ' / redis start' });
+  } catch (err) {
+    console.log('redis error');
+    await webhook.send({ content: new Date().toISOString() + ' / redis error' });
+  }
+
+  // cron
+  const cron = require('./cron');
+
+  // port open
+  try {
+    const main_cache = await redis.get('GET:MARKETS');
+    if (!main_cache) {
+      // market update
+      const Coin = require('./modules/coin');
+      await Coin.updateAllMarketHistories();
+    }
+
+    app.listen(app.get('port'), async () => {
+      console.log(app.get('port'), 'port server is running!');
+      await webhook.send({ content: new Date().toISOString() + ' / server start' });
+    });
+  } catch (err) {
+    console.log('server error');
+    await webhook.send({ content: new Date().toISOString() + ' / server error' });
+  }
+})();
