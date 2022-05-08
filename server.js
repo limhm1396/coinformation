544a4d10abb77cbc0b5448b7bc21e88537edda09
Jake Lim <limhm1396@naver.com>
2022-05-08 13:42:54 +0900

    server 실행 조건 분기 - PRD일 때만 디스코드 알림

--- server.js
+++ server.js
@@ -8,18 +8,24 @@ const webhook = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_SERVER_REST
 const { sequelize } = require('./models');
 const redis = require('./redis');
 
+const PRD = 'prd';
+
 // server start
 (async () => {
   try {
     // db connect
     await sequelize.sync({ force: false, alter: false });
     console.log('DB start');
-    await webhook.send({ content: new Date().toISOString() + ' / DB start' });
-    
+    if (process.env.NODE_ENV === PRD) {
+      await webhook.send({ content: new Date().toISOString() + ' / DB start' });
+    }
+
     // redis connect
     await redis.connect();
     console.log('redis start');
-    await webhook.send({ content: new Date().toISOString() + ' / redis start' });
+    if (process.env.NODE_ENV === PRD) {
+      await webhook.send({ content: new Date().toISOString() + ' / redis start' });
+    }
 
     // cron
     const cron = require('./cron');
@@ -34,11 +40,15 @@ const redis = require('./redis');
 
     app.listen(app.get('port'), async () => {
       console.log(app.get('port'), 'port server is running!');
-      await webhook.send({ content: new Date().toISOString() + ' / server start' });
+      if (process.env.NODE_ENV === PRD) {
+        await webhook.send({ content: new Date().toISOString() + ' / server start' });
+      }
     });
   } catch (err) {
     console.log('ERROR');
-    await webhook.send({ content: new Date().toISOString() + ' / ERROR' });
-    pm2.restart('server');
+    if (process.env.NODE_ENV === PRD) {
+      await webhook.send({ content: new Date().toISOString() + ' / ERROR' });
+      pm2.restart('server');
+    }
   }
 })();
