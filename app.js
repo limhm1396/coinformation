26cbbbda71f5195b911bf6e8fd66fc0eb7823b63
Jake Lim <limhm1396@naver.com>
2022-05-08 13:44:33 +0900

    404 페이지 추가 - HTTP code 404 페이지 추가

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.render('not_found');
 });
 
 // error handler
